36153d7edffe90b146cbe2a1df900a91d7021006
kmeixner <kmeixner42@gmail.com>
2021-02-14 22:09:27 -0500

    Now a react-select component with employees as option
    modified:   client/src/SelectStaffMember.js

--- client/src/SelectStaffMember.js
+++ client/src/SelectStaffMember.js
@@ -1,4 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react'
+import Select from 'react-select';
+
+const options = [
+  { value: 'X1', label: 'X1' },
+  { value: 'X2', label: 'X2' },
+  { value: 'X3', label: 'X3' },
+  { value: 'X4', label: 'X4' },
+  { value: 'X5', label: 'X5' },
+  { value: 'X6', label: 'X6' },  
+  { value: 'X7', label: 'X7' }
+]
 
 class SelectStaffMember extends React.Component {
 
@@ -7,7 +18,7 @@ class SelectStaffMember extends React.Component {
   }
 	
   render() {
-    return <div>X</div>;
+    return <Select options={options} />;
   }
 }
 
