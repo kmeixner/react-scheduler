c91d5602f2fe575b16e07f1484ac13ae87248bc4
kmeixner <kmeixner42@gmail.com>
2021-02-14 23:01:55 -0500

    <SelectStaffMember> mostly works... ...mostly...
    modified:   client/src/SelectStaffMember.js

--- client/src/SelectStaffMember.js
+++ client/src/SelectStaffMember.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Select from 'react-select';
 
 const options = [
+  { value: 'none', label: 'None' },
   { value: 'X1', label: 'X1' },
   { value: 'X2', label: 'X2' },
   { value: 'X3', label: 'X3' },
@@ -15,10 +16,15 @@ class SelectStaffMember extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {employee: ''};
   }
+  
+	handleChange = (event) => {
+	  this.setState({employee: event.value});
+	}
 	
   render() {
-    return <Select options={options} />;
+    return <Select options={options} onChange={this.handleChange.bind(this)} />;
   }
 }
 
